31e2ec7df75b7fa9d40796d8f02cd0bc2cb06071
Jonathan Hsu <jhsu@twilio.com>
2013-07-12 14:55:00 -0700

    final texting version

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from twilio.rest import TwilioRestClient
 from flask import Flask, request, render_template
+import time
  
 twilio_from_number = '6199425133'
-twilio_account_sid = 'AC9853752e40599cb7fc11134c166c6866'
-twilio_auth_token = '5415da2396c51ffb05d8e84938a9fef1'
+# put in your account sid and token
+twilio_account_sid = ''
+twilio_auth_token = ''
  
  
 app = Flask(__name__)
@@ -21,35 +23,47 @@ def index():
         
         message = client.sms.messages.create(to=request.form['phone_number'], from_= twilio_from_number, body="Hi " + recipient + " your friend " +name + " is calling with a special message")        
         
+        time.sleep(1)
+
         client.sms.messages.create(
              to=request.form['phone_number'],
              from_=twilio_from_number,
              body='http://images.wikia.com/dragonball/images/f/f3/Rick_astley.gif')
+
+        time.sleep(1)
 
         client.sms.messages.create(
              to=request.form['phone_number'],
              from_=twilio_from_number,
              body='We\'re no strangers to love, You know the rules and so do I, A full commitment\'s what I\'m thinking of, You wouldn\'t get this from any other guy')
 
+        time.sleep(1)
+
         client.sms.messages.create(
              to=request.form['phone_number'],
              from_=twilio_from_number,
              body='I just wanna tell you how I\'m feelin. Gotta make you understand. ')
+
+        time.sleep(1)
 
         client.sms.messages.create(
              to=request.form['phone_number'],
              from_=twilio_from_number,
              body='Never gonna give you up. Never gonna let you down. Never gonna run around and desert you. ')
 
+        time.sleep(1)
+
         client.sms.messages.create(
              to=request.form['phone_number'],
              from_=twilio_from_number,
              body='Never gonna make you cry. Never gonna say goodbye. Never going to tell a lie, and hurt you.')
 
+        time.sleep(1)
+
         client.sms.messages.create(
              to=request.form['phone_number'],
              from_=twilio_from_number,
-             body='Sent with love, using Twilio.  If you want to rickroll your friends, click [URL HERE]')
+             body='Sent with love, using Twilio.  If you want to rickroll your friends, visit http://sallysapp.herokuapp.com/')
         print "sent"
     return render_template('simple.html')
  
